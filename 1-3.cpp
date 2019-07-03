c83e93eb6839f995569215f633f3b8839eedc947
runyud <runyud@umich.edu>
2019-07-03 10:18:05 -0400

    second

---
+++ 1-3.cpp
@@ -0,0 +1,28 @@
+//
+//
+//  CTCI questions
+//  1.1 IsUnique
+//
+//
+
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+string URLify(string str, int len){
+	for(int i = len - 1; i >= 0; i--){
+		if(str[i] == ' '){
+			str.replace(i, 1, "%20");
+		}
+	}
+	return str;
+}
+
+int main(){
+	string str1 = "Mr John Smith";
+	string str2 = URLify(str1, 13);
+	cout << str2 << endl;
+	return 0;
+}
